eda2786bd37ef9eed42ab5dd8c13a35dde07562b
rafaellevissa <rafaellevissa@gmail.com>
2024-05-13 01:03:07 -0300

    Enable cors

--- functions/api/task/index.ts
+++ functions/api/task/index.ts
@@ -5,10 +5,15 @@ import getTask from './get-task';
 import createTask from './create-task';
 import deleteTask from './delete-task';
 import updateTask from './update-task';
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
+}))
 
 app.get('/task', listTask);
 app.get('/task/:id', getTask);
